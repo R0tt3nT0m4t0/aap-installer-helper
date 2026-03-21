218272bc64d34d742e2231cba84ada000f78d2c2
Alex-MeDel <isalexmedel@gmail.com>
2026-03-21 15:27:41 -0500

    Added a few comments and also copy/paste the pre-setup.yml to this directory for ease of use I guess

--- terraform-growth/main.tf
+++ terraform-growth/main.tf
@@ -11,7 +11,8 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-# 1. The Base Cloud Image
+# 1. The Base Cloud Image, this block can be changed to any other linux cloud image, as long as it is in the same specified directory
+# Decided to go with almalinux instead of RHEL (original code) due to login requirement to download RHEL images, this can be changed to RHEL with minimum changes to code
 resource "libvirt_volume" "rhel_base" {
   name   = "almalinux-9-base.qcow2"
   pool   = "default"
@@ -20,7 +21,7 @@ resource "libvirt_volume" "rhel_base" {
 }
 
 # 2. The VM Specific Disk
-# Clones the base image and expands it to the requested 40 GiB.
+# Clones the base image and expands it to the requested 40 GiB. (can adjust later with another terraform apply)
 resource "libvirt_volume" "os_disk" {
   name           = "aap26_disk.qcow2"
   pool           = "default"
@@ -37,10 +38,11 @@ resource "libvirt_cloudinit_disk" "commoninit" {
 }
 
 # 4. The Virtual Machine Definition
+# NOTE: Both the memory and the vpcu can be changed with a terraform apply
 resource "libvirt_domain" "aap_vm" {
   name   = "aap26"
   memory = 8192
-  vcpu   = 4
+  vcpu   = 4 
 
   cpu {
     mode = "host-passthrough"
